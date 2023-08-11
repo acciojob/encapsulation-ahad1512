29bbf89f88c1b21e41216776e2651d183770e662
ahad1512 <ahadk747@gmail.com>
2023-08-12 01:14:29 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+//        rwOnly.name = "Ahad";
+//        System.out.println(rwOnly.name);
+//        name has private access in com.driver.RWOnly
+        rwOnly.setName("Ahad");
+        rwOnly.getName();
+    }
 }
